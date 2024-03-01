80f02f965ee936f01ce5cbd25be648d5544a5525
Shoxrux Yuldashov <deagleshox@mail.ru>
2024-03-01 16:00:13 +0000

    ДЗ 05.03.2024
    - "Поле Чудес"
    - "Музыкальный чарт"

--- guess_num_bot.py
+++ guess_num_bot.py
@@ -1,2 +1,33 @@
-# TODO: Реализовать игру "Угадай число" версию, когда
-#  пользователь загадывает число, а программа будет угадывать.
+START: int = 1
+END: int = 100
+LIFE: int = 7
+
+
+def game_with_bot():
+    low, high = START, END
+
+    print(f'Загадайте число от {START} до {END}'
+          '\nЯ буду угадывать число Вы должны ответить '
+          '"больше", "меньше" или "угадал" если я угадал Ваше число')
+
+    attempt: int = 0
+    while attempt < LIFE:
+        attempt += 1
+
+        guess: int = (low + high) // 2
+        print(f'Бот: мой вариант - {guess}')
+        user_ans: str = input('Бот: Это Ваше число?: ').lower()
+
+        if user_ans == 'угадал':
+            return f'Бот: Бот угадал Ваше число за {attempt} попыток'
+
+        if user_ans == 'больше':
+            low = guess - 1
+        elif user_ans == 'меньше':
+            high = guess + 1
+        else:
+            print('Пожалуйста, введите "больше", "меньше" или "угадал"')
+    return 'Бот: Бот проиграл не смог угадать Ваше число'
+
+
+print(game_with_bot())
