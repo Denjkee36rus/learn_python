c420893a5b2ccf77ed81dc35a53e291fd8d4a5c4
dchekalin <chekalkin_92@mail.ru>
2024-03-07 15:35:02 +0300

    test commit
    - удалены ненужные строки кода

--- field_of_wonders.py
+++ field_of_wonders.py
@@ -3,13 +3,24 @@ import random
 WORDS: list[str] = [
     'делить', 'ессентуки', 'накопать', 'панический', 'штопать'
 ]
-# WORD: str = ...  # используйте метод из модуля random
-# WORD: list[str] = ['х', 'о', 'р', 'о', 'ш', 'о']
+random_word: str = random.choice(WORDS) # выбираем случайное слово
 
-WORD: str = 'хорошо'
+WORD: list[str] = list(random_word) # Делаем из случайного слова, список букв ['ш', 'т', 'о', 'п', 'а', 'т', 'ь']
+
+
+stars: list[str] = list('*' * len(WORD)) # Подготавливаю список одинаковой длины, для сравнения с WORD list[]
+letter = input('Какая буква?: ')  # о
+for i in range(WORD):
+    if letter in WORD:
+        pass
+
+
+
+
+
 # stars: list = ['*', '*', '*', '*', '*', '*']
 
-letter = input('Какая буква?: ')  # о
 
 # TODO: Проверить длину введенной строки, больше одной буквы указывать нельзя
-print('*и*')
+print(WORD)
+print(stars)
