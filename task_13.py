206c5e9172c237d6adf71edbb5d806d0f881b454
dchekalin <chekalkin_92@mail.ru>
2024-03-12 17:53:58 +0300

    13 задача
    - удалены ненужные строки кода

---
+++ task_13.py
@@ -0,0 +1,16 @@
+# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+
+names = "ЕгорКрид,КлаваКока,МаксБарских,Niletto,Artik&Asti".split(",")
+tasks = 'Niletto +1,КлаваКока -2,ЕгорКрид -3'.split(',')
+
+
+for artist in tasks:
+    name, delta = artist.split()
+    names.insert(names.index())
+
+
+    
+
+
+
+
