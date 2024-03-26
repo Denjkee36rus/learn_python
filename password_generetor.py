7293aa9cbd1676cc8d2b377b4a8cc318836afc6e
dchekalin <chekalkin_92@mail.ru>
2024-03-26 17:43:26 +0300

    Генератор паролей
    - удалены ненужные строки кода

--- password_generetor.py
+++ password_generetor.py
@@ -5,12 +5,12 @@ lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
 uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 punctuation = '!#$%&*+-=?@^_'
 
-count_password = int(input('Введите необходимое вам количество паролей'))
-length_password = int(input('Введите длину пароля, от 6 до 20 символов'))
-including_numbers = input('Включать ли цифры 0123456789 ?, yes/no')
-including_uppercase = input('Включать ли заглавные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?, yes/no')
-including_lowercase = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?, yes/no')
-including_chars = input('Включать ли символы!#$%&*+-=?@^_? (y/n)?, yes/no')
+count_password = int(input('Введите необходимое вам количество паролей '))
+length_password = int(input('Введите длину пароля, от 6 до 20 символов '))
+including_numbers = input('Включать ли цифры 0123456789 ?, yes/no ')
+including_uppercase = input('Включать ли заглавные буквы ABCDEFGHIJKLMNOPQRSTUVWXYZ?, yes/no ')
+including_lowercase = input('Включать ли строчные буквы abcdefghijklmnopqrstuvwxyz?, yes/no ')
+including_chars = input('Включать ли символы!#$%&*+-=?@^_? (y/n)?, yes/no ')
 
 
 def check_length_password():
@@ -40,48 +40,51 @@ def entered_user_data():
 
 def generate_user_password():
     # TODO: Подсказка 2. Значит у Вас здесь будет другой тип данных
+    list_password = []
     final_password = ''
     if check_length_password():
         for _ in range(count_password):
             for i in range(length_password):
                 final_password += random.choice(entered_user_data())
-        print(final_password, sep='\n')
+            list_password.append(final_password)
+            final_password = ''
+        print(*(list_password), sep='\n')
 
 
-print(generate_user_password())
+generate_user_password()
 
 
 # TODO: Подсказка 1.
 #  Представьте задачу: Сгенерируйте указанное количество случайных чисел:
 
 # TODO: Решение:
-def get_random_number(count: int, start: int, end: int) -> list[int] | None:
-    """
-    Сгенерирует указанное количество случайных чисел.
-
-    :param count: Количество чисел.
-    :param count: Начало диапазона.
-    :param count: Конец диапазона.
-    """
-
-    # Обработка ошибочных моментов
-    if count <= 0:
-        print('Укажите натуральное число')
-        return
-
-    # Обработка ошибочных моментов
-    if start > end:
-        print('Начало диапазона должно быть меньше конца')
-        return
-
-    result = []
-    while count > 0:
-        rand_num = random.randint(start, end)
-        result.append(rand_num)
-
-        count -= 1
-
-    return result
-
-
-print(get_random_number(5, 100, 150))
+# def get_random_number(count: int, start: int, end: int) -> list[int] | None:
+#     """
+#     Сгенерирует указанное количество случайных чисел.
+#
+#     :param count: Количество чисел.
+#     :param count: Начало диапазона.
+#     :param count: Конец диапазона.
+#     """
+#
+#     # Обработка ошибочных моментов
+#     if count <= 0:
+#         print('Укажите натуральное число')
+#         return
+#
+#     # Обработка ошибочных моментов
+#     if start > end:
+#         print('Начало диапазона должно быть меньше конца')
+#         return
+#
+#     result = []
+#     while count > 0:
+#         rand_num = random.randint(start, end)
+#         result.append(rand_num)
+#
+#         count -= 1
+#
+#     return result
+#
+#
+# print(get_random_number(5, 100, 150))
